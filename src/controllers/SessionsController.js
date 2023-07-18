2f98f47b33d447e3131f1e403cb73a91d0efc0c8
João Paulo <joao.paulomonteiro@hotmail.com>
2023-07-18 15:15:18 -0300

    Password validation in the database

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError")
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcrypt")
 
 class SessionsController {
   async create(req, res) {
@@ -9,6 +10,12 @@ class SessionsController {
 
     if(!user) {
       throw new AppError("E-mail e/ou senha incorreta.", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if(!passwordMatched) {
+      throw new AppError("Email e/ou senha incorreta.", 401)
     }
 
     return res.json(user)
