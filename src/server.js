c1713e6eb5eeb148329a48236c340291d810826b
João Paulo <joao.paulomonteiro@hotmail.com>
2023-08-02 14:41:21 -0300

    starting to use the restful api

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 require('express-async-errors')
 
 const migrationsRun = require('./database/sqlite/migrations');
-
 const AppError = require('./utils/AppError');
+const uploadConfig = require("./configs/upload")
 
+const cors = require("cors")
 const express = require('express');
 const routes = require('./routes');
 
 migrationsRun();
 
 const app = express();
-app.use(express.json())
 
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+app.use(routes);
 
 app.use((error, req, res, next ) => {
   if(error instanceof AppError) {
